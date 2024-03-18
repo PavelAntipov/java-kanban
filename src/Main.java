08ac51d4e481cc9b615220876ffcf2973e5319c5
pavel <enrique_up@mail.ru>
2024-03-18 19:55:18 +0300

    поправил main, убрал переменную в метод main

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    static TaskManager taskManager = new TaskManager();
+
 
     public static void main(String[] args) {
-
+        TaskManager taskManager = new TaskManager();
     }
 }
 
