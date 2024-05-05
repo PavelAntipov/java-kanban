e9d722f3ba903ee6e7d5fd4245b4145a04fc9238
pavel <enrique_up@mail.ru>
2024-05-05 15:23:09 +0300

    code style fixes

--- src/issues/Epic.java
+++ src/issues/Epic.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class Epic extends Task {
     private ArrayList<Integer> subtaskList;
+
     public Epic(String name, String description) {
         super(name,description);
         this.subtaskList = new ArrayList<>();
@@ -30,11 +31,13 @@ public class Epic extends Task {
     public void deleteSubtaskFromList(int issId) {
         subtaskList.remove(issId);
     }
+
     @Override
     public String toString() {
         return "id: " + id + " Название: " + name + " Статус: " + status +
                 " Описание: " + description + " Список подзадач: " + subtaskList + "\n";
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, description, id, status,subtaskList);
