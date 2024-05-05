30fffff8ce5da0654828029f2250e5c385fa245b
pavel <enrique_up@mail.ru>
2024-05-05 15:47:45 +0300

    delete non-actual history manager tests

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -11,21 +11,7 @@ class InMemoryHistoryManagerTest {
 
     TaskManager taskManager = Managers.getDefaultTaskManager();
 
-    @Test
-    public void ManagerCanAddAndSearchTasks() {
-        Task task1 = new Task("name1", "description1");
-        taskManager.taskCreate(task1);
 
-        Task task = taskManager.getTaskById(1);
-        Task task2 = new Task("name1", "description2");
-        task2.setId(1);
-        taskManager.taskUpdate(task2);
-
-        task = taskManager.getTaskById(1);
-        Task history1 = taskManager.getHistoryManager().get(0);
-        Task history2 = taskManager.getHistoryManager().get(1);
-        Assertions.assertNotEquals(history1.hashCode(), history2.hashCode());
-    }
     //проверка того, что история пишется и порядок при повторном просмотре тасок меняется
     @Test
     public void historyManagerWritesHistory() {
