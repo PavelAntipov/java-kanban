de476df28c9f900a7c5bc1f61bcc3c1adc508acb
pavel <enrique_up@mail.ru>
2024-05-05 15:30:47 +0300

    code style fixes

--- src/Managers.java
+++ src/Managers.java
@@ -1,9 +1,13 @@
 public class Managers {
-    private Managers() {}
+    private Managers() {
+
+    }
+
     public static TaskManager getDefaultTaskManager() {
         return new InMemoryTaskManager();
     }
-    public static HistoryManager getDefaultHistoryManager(){
+
+    public static HistoryManager getDefaultHistoryManager() {
         return  new InMemoryHistoryManager();
     }
 }
